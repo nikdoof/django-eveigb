e2606e35fd4192bc33af9443d8ddb0478ef1406e
Andrew <andy@tensixtyone.com>
2013-10-13 16:56:28 +0100

    Add tag to link EVE mail either in-game or out of game.

--- eveigb/templatetags/igb.py
+++ eveigb/templatetags/igb.py
@@ -1,3 +1,13 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
+
+
+@register.inclusion_tag('eveigb/open_mail.html', takes_context=True)
+def eve_mail_link(context):
+    """ Provide a link either to in-game mail, or EVE Gate mail"""
+    return {
+        'igb': context['is_igb'],
+        'gate_base': getattr(settings, 'EVE_GATE_BASE', 'https://gate.eveonline.com')
+    }
